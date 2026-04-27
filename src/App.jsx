4df9ba8ab0598f6bfac2aaa0c65e2bd3399c4625
Oleksiy Blavat <blavat.alex@gmail.com>
2026-04-27 02:53:24 +0200

    conditional redering - initial pass

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,25 @@ import { useState } from "react";
 
 export default function App() {
   const [count, setCount] = useState(0);
+  let show = false;
+  let showlow = false;
+  if (count >= 10) {
+    show = true;
+    showlow = false;
+  }
+
+  if (count < 0){
+    showlow = true;
+    show = false;
+  }
+
 
   return (
       <div style={{ padding: 20 }}>
         <h1>Counter</h1>
 
         <p>Count is: {count}</p>
+
 
         <button onClick={() => setCount(count + 1)}>
           Increment
@@ -20,6 +33,8 @@ export default function App() {
         <button onClick={() => setCount(0)}>
           RESET ALL
         </button>
+        {show && <p>TOO HIGH 🔥</p>}
+        {showlow && <p>too low 🥶</p>}
       </div>
   );
 }
